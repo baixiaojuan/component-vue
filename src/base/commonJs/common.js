0a595df63d0f837a5090236bb8cbace1c7547a08
baixiaojuan <baixj@golive.cc>
2020-07-08 10:55:01 +0800

    '公共组件调试'

---
+++ src/base/commonJs/common.js
@@ -0,0 +1,9 @@
+const common = {
+    getIndex: function(target) {
+        return 0
+    },
+    getNumber: function(target) {
+        return 1
+    },
+}
+export default common
--- src/base/index.js
+++ src/base/index.js
@@ -1,11 +1,23 @@
-import loading from './component/loading.vue'
-const components = {
-    install(Vue){
-       Vue.component( 'loading', loading)
-    }
-    }
-    if(typeof window !== 'undefined' && window.Vue) {
-       window.Vue.use(components)
-    }
-    //暴露接口
-    export default components
+import Loading from './component/loading.vue'  //UI组件库
+import common from './commonJs/common'  //js插件、数据处理方法
+const components = [Loading]
+const install = function(Vue, opts = {}) {
+   components.forEach(component => {
+     Vue.component(component.name, component);
+   });
+ };
+ if(typeof window !== 'undefined' && window.Vue) {
+   install(window.Vue);
+ }
+ //暴露接口
+ export {install,Loading,common}
+
+
+//组件引用只要在main.js里面引用
+//  例如: import {Loading} from 'component-vue' Vue.component('loading', Loading); Vue.use(Loading)
+
+//js插件引用:在需要的文件页面引用
+// 例如：import {common } from 'component-vue'
+
+
+
